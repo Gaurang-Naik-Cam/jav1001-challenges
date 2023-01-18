c1de77d0ca171dc571fc2247a05caee025f5bf7a
Gaurang Naik <A00250808@mycambrian.ca>
2023-01-17 20:32:50 -0500

    Update Main.kt
    Printing Fun calls

--- Main.kt
+++ Main.kt
@@ -3,10 +3,12 @@ package challenges
 import challenges.testers.*
 
 fun main() {
+	println("fun started")
 	//SortedSquaresTester.run()
 	//TwoNumSumTester.run()
 	//ThreeLargestTester.run()
 	//GotAnyChangeTester.run()
 	//MinWaitTimeTester.run()
 	//runLengthEncodingTester.run()
+	print("fun finished")
 }
